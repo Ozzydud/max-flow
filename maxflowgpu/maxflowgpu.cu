df020d6514e77d4ac250f25668ae23a4405f0686
Jepzen55 <j_schrum@hotmail.com>
2024-03-18 12:14:22 +0100

    added imports

--- maxflowgpu/maxflowgpu.cu
+++ maxflowgpu/maxflowgpu.cu
@@ -1,6 +1,14 @@
 #include <iostream>
 #include <cuda_runtime.h>
 #include <chrono>
+#include <limits.h>
+#include <queue>
+#include <string.h>
+#include <fstream>
+#include <vector>
+using namespace std;
+
+#define V 6;
 
 // Perform bfs to find augmenting paths
 __global__ void CudaBfs(vector<vector<int>>& rGraph, int s, int t, vector<int>& parent){
