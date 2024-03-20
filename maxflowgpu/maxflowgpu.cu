b851b1efb39132273f8ba894ea2b8a62950bb98d
Jepzen55 <j_schrum@hotmail.com>
2024-03-20 12:17:37 +0100

    esing

--- maxflowgpu/maxflowgpu.cu
+++ maxflowgpu/maxflowgpu.cu
@@ -6,7 +6,7 @@
 #include <cuda_runtime.h>
 #include <device_launch_parameters.h>
 
-using namespace std;
+#define INF 1e9
 
 // BFS
 __global__ void cudaBFS (int *row, int *indices, int *data,
@@ -22,6 +22,7 @@ __global__ void cudaBFS (int *row, int *indices, int *data,
      __syncthreads(); // Not optimal - we need to wait for all threads before we do BFS
 
      while (!visited[sink] && !visited[source]) { //We keep going as long as we have not visited both sink and source
+            // Needs changing to fit with our data ---- ALL OF THE BELOW
             for (int i = row[tid]; i < row[tid + 1]; ++i) {
             int v = indices[i]; // Get the destination vertex
             if (!visited[v] && residual[i] > 0) {
@@ -40,7 +41,26 @@ __global__ void cudaBFS (int *row, int *indices, int *data,
 __global__ void augmentPath(int *residual, int *parent, int *flow, 
                             int vertices, int source, int sink){
     int tid = blockIdx.x * blockDim.x * threadIdx.x; //Finding thread ID
-    
+    int vertices = 5;
+    if(tid<vertices && parent[tid] != -1){ //if == -1, it was not reached in BFS
+        int min_flow = INF;
+        int current = tid;
+        while (current != source) {
+            int current_parent = parent[current];
+            // Needs changing to follow data structure
+            min_flow = min(min_flow, residual[current_parent * vertices + current]);
+            current = current_parent;
+        }
+
+        int current = tid;
+        while(current != source){
+            int current_parent = parent[current];
+            residual[current_parent * vertices + current] -= min_flow;
+            residual[current * vertices + current_parent] += min_flow;
+            current = current_parent;
+        }
+        flow[tid] += min_flow;
+    }
 }
 
 
