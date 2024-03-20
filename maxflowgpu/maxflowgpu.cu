b4cce81e6881da739bf4d696a2b0d74cc73e7273
Jepzen55 <j_schrum@hotmail.com>
2024-03-20 11:55:17 +0100

    Fixing code

--- maxflowgpu/maxflowgpu.cu
+++ maxflowgpu/maxflowgpu.cu
@@ -12,8 +12,7 @@ using namespace std;
 __global__ void cudaBFS (std::vector<int>& row, std::vector<int>& indices, std::vector<int>& data,
                          int source, int sink, int *parent, int *queue, int *flow, int *residual, bool *visited){
     int tid = blockIdx.x * blockDim.x * threadIdx.x; //Finding thread ID
-    int vertices = row.size()-1;
-
+    int vertices = 5;
     if(visited[tid] == false && vertices > tid){ //Mark as visited and add tid to the queue
         queue[tid] = tid;
         visited[tid] = true;
