a8ace5b7310ec87188eea3b1a9972b2a04a71436
Jepzen55 <j_schrum@hotmail.com>
2024-04-08 11:40:10 +0200

    121

--- maxflowcpu/maxflowcpu.cpp
+++ maxflowcpu/maxflowcpu.cpp
@@ -170,7 +170,7 @@ int main()
     }
 
     // Read the graph from .mtx file
-    const char *filename = "data/gre_1107.mtx";
+    const char *filename = "data/cage9.mtx";
     int total_nodes = V;
 
     readInput(filename, total_nodes, graph);
