6a7763f23d28d502b275015130ed5eba7aa69e0b
Jepzen55 <au691389@uni.au.dk>
2024-05-13 10:43:32 +0200

    124

--- maxflowgpu/maxflowgpuBU.cu
+++ maxflowgpu/maxflowgpuBU.cu
@@ -72,12 +72,12 @@ void readInput(const char* filename, int total_nodes, int* residual) {
 
 __global__ void cudaBFS(int *r_capacity, int *parent, int *flow, bool *frontier, bool* visited, int vertices, int source, int* locks){
     int Idx = blockIdx.x * blockDim.x + threadIdx.x;
-    if (!frontier[source] && Idx < vertices && frontier[Idx]) {
-        printf("%d ", frontier[0]);
+    printf("%d ", frontier[0]);
         printf("%d ", frontier[1]);
         printf("%d ", frontier[2]);
         printf("%d ", frontier[3]);
         printf("%d ", frontier[4]);
+    if (!frontier[source] && Idx < vertices && frontier[Idx]) {
         frontier[Idx] = false;
         visited[Idx] = true;
 
