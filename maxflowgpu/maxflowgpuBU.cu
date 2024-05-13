aa618aee30ae1f3f9f3a997353e717965f7a3e7d
Jepzen55 <au691389@uni.au.dk>
2024-05-13 16:44:01 +0200

    24124

--- maxflowgpu/maxflowgpuBU.cu
+++ maxflowgpu/maxflowgpuBU.cu
@@ -274,9 +274,6 @@ int edmondskarp(const char* filename, int total_nodes) {
         avgBFSTime += bfsmili;
 
         cudaMemcpy(frontier, d_frontier, total_nodes * sizeof(bool), cudaMemcpyDeviceToHost);
-        if(!found_augmenting_path){
-            break;
-        }
         }
 
         found_augmenting_path = source_reachable(frontier, total_nodes, source);
