8a20a88e1521e7a8e7ea5668e0220579d12ddd91
Jepzen55 <au691389@uni.au.dk>
2024-05-13 20:53:23 +0200

    123

--- maxflowgpu/maxflowgpuBU.cu
+++ maxflowgpu/maxflowgpuBU.cu
@@ -314,7 +314,7 @@ int edmondskarp(const char* filename, int total_nodes) {
 	counter++;
 	//cout << "Counter is: " << counter << endl;
 
-    } while(counter != 3); //found_augmenting_path);
+    } while(found_augmenting_path); //found_augmenting_path);
     cout << "Counter is: " << counter << endl;
     // Record stop time
     cudaEventRecord(stop);
