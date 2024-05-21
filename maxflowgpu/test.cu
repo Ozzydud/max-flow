ab1c8f3fdc592f162e2d14936f2afc7a26182f14
Jepzen55 <au691389@uni.au.dk>
2024-05-21 11:43:20 +0200

    23

--- maxflowgpu/test.cu
+++ maxflowgpu/test.cu
@@ -277,7 +277,7 @@ float edmondskarp(const char* filename, int total_nodes) {
 
         cudaMemcpy(flow, d_flow, total_nodes * sizeof(int), cudaMemcpyDeviceToHost);
         cudaMemcpy(parent, d_parent, total_nodes * sizeof(int), cudaMemcpyDeviceToHost);
-        if(did_use_BU){
+        if(!did_use_BU){
             path_flow = flow[source];
             max_flow += path_flow;
             cout << max_flow << endl;
